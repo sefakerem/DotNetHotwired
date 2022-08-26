cace1936586e3d3f88f9550f5e7ae8cbf34cb6db
Sefa Kocakoglu <sefa.kocakoglu@idesse.com.tr>
2022-08-26 09:05:45 +0300

    derste konuşulan bazı işlemler denendi

--- Pages/Home.cshtml.cs
+++ Pages/Home.cshtml.cs
@@ -22,15 +22,15 @@ namespace Hotwired.Pages
             return Partial("CardTypeAddEdit", type);
 
         }
-        public PartialViewResult OnPostSaveType(int id, string descr, bool isVisit, bool isUserAccount, bool forcedLocation)
+        public IActionResult OnPostSaveType(int id, string descr, bool isVisit, bool isUserAccount, bool forcedLocation)
         {
             if (id == 0)
             {
                 var type = new CardType { Id = Types.Instance.Count + 1, Descr = descr, IsVisit = isVisit, ForcedLocation = forcedLocation, IsUserAccount = isUserAccount };
                 Types.Instance.Add(type);
 
-                Response.ContentType = "text/vnd.turbo-stream.html";
-                return Partial("CardTypeAdd", type);
+                
+                
             }
             else
             {
@@ -39,10 +39,10 @@ namespace Hotwired.Pages
                 type.IsVisit = isVisit;
                 type.IsUserAccount = isUserAccount;
                 type.ForcedLocation = forcedLocation;
+                
 
-                Response.ContentType = "text/vnd.turbo-stream.html";
-                return Partial("CardTypeEdit", type);
             }
+            return RedirectToPage("CardType");
         }
 
         public PartialViewResult OnGetEditType(int id)
